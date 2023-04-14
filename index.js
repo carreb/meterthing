bfc0dd601704c82f83828cabfa30105367ccfa94
carreb <caleb@domyjobfor.me>
2023-04-13 23:06:30 -0400

    add back the click command to the meter message, also shows percentage in hover

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ register("chat", (message, event) => {
             // get the amount of xp from the message using regex
             // the message is formatted like this: "   RNG Meter - 209,000 Stored XP"
             // so we need to get the number and remove the commas
+            let originalMessage = new Message(event)
+            let clickaction = originalMessage.getMessageParts()[0].getClickValue()
             let xp = message.match(/\d{1,3}(?:,\d{3})*(?:\.\d+)?/g)[0];
             xpStripped = parseInt(xp.replace(/[,]/g,''))
             let coreChance = (0.0565 * (1 + (2 * xpStripped/885000)))
             let coreChanceMagicFind = (coreChance * (1 + (pog.magic_find / 100)))
+            let xpPercentage = xpStripped / 885000;
+            let meterMessage = new TextComponent("   &dRNG Meter &f- &d" + xp + "/885,000 Stored XP &8(" + Math.floor(xpStripped / 500) + "/" + 885000 / 500 + ")").setClick("run_command", `${clickaction}`).setHoverValue(`&7Your RNG meter is &d${(xpPercentage * 100).toFixed(2)}% &7filled!\n&7Click to open RNG Meter menu.`);
             cancel(event)
-            ChatLib.chat("   &dRNG Meter &f- &d" + xp + "/885,000 Stored XP &8(" + Math.floor(xpStripped / 500) + "/" + 885000 / 500 + ")");
+            ChatLib.chat(meterMessage);
             ChatLib.chat("   &7↪ &6Judgement Core &7Chance: &b" + coreChanceMagicFind.toFixed(4) + "% +" + pog.magic_find + "✯ &8(" + coreChance.toFixed(4) + "% base)");
     }
     }
@@ -27,7 +31,6 @@ register("chat", (message, event) => {
 
 register("chat", (message, event) => {
     // check if the message is a rare drop using regex
-    console.log(message)
     if (message.match(/^(RARE DROP!|VERY RARE DROP!|INSANE DROP!|CRAZY RARE DROP!)\s\(/g)) {
         let itemName = message.match(/\(([^)]+)\)/g)[0];
         let magicFind = message.match(/\(([^)]+)\)/g)[1];
