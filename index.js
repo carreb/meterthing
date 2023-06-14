cde2c90a8ab175dd8a304817ce77b45f58729f4c
carreb <caleb@domyjobfor.me>
2023-06-13 20:08:28 -0400

    make the last fix make more sense and also just work better in general

--- index.js
+++ index.js
@@ -9,28 +9,15 @@ let lastXP
 
 
 register("chat", (message, event) => {
+    let doingVoidgloom = false
+    for(let i = 0; i < Scoreboard.getLines().length; i++) {
+        if (Scoreboard.getLineByIndex(i).getName().includes("Voidgloom") && Scoreboard.getLineByIndex(i).getName().includes("IV")) {
+            doingVoidgloom = true
+        }
+    }
     if (message.startsWith("   RNG Meter -")) {
         console.log(Scoreboard.getLineByIndex(5).getName())
-        if (Scoreboard.getLineByIndex(3).getName().includes("Voidgloom") && Scoreboard.getLineByIndex(3).getName().includes("IV")) {
-            // get the amount of xp from the message using regex
-            // the message is formatted like this: "   RNG Meter - 209,000 Stored XP"
-            // so we need to get the number and remove the commas
-            let originalMessage = new Message(event)
-            let clickaction = originalMessage.getMessageParts()[0].getClickValue()
-            let xp = message.match(/\d{1,3}(?:,\d{3})*(?:\.\d+)?/g)[0];
-            xpStripped = parseInt(xp.replace(/[,]/g,''))
-            lastXP = xpStripped
-            let coreChance = (0.0565 * (1 + (2 * xpStripped/885000)))
-            let coreChanceMagicFind = (coreChance * (1 + (pog.magic_find / 100)))
-            let xpPercentage = xpStripped / 885000;
-            let meterMessage = new TextComponent("   &dRNG Meter &f- &d" + xp + "/885,000 Stored XP &8(" + Math.floor(xpStripped / 500) + "/" + 885000 / 500 + ")").setClick("run_command", `${clickaction}`).setHoverValue(`&7Your RNG meter is &d${(xpPercentage * 100).toFixed(2)}% &7filled!\n&7Click to open RNG Meter menu.`);
-            cancel(event)
-            ChatLib.chat(meterMessage);
-            let magicFindMessage = new Message("   &7↪ &6Judgement Core &7Chance: &b" + coreChanceMagicFind.toFixed(4) + "% +" + pog.magic_find + "✯ &8(" + coreChance.toFixed(4) + "% base)");
-            magicFindMessage.chat()
-            lastMagicFindMessage = magicFindMessage
-        }
-        else if (Scoreboard.getLineByIndex(5).getName().includes("Voidgloom") && Scoreboard.getLineByIndex(5).getName().includes("IV")) {
+        if (doingVoidgloom) {
             // get the amount of xp from the message using regex
             // the message is formatted like this: "   RNG Meter - 209,000 Stored XP"
             // so we need to get the number and remove the commas
